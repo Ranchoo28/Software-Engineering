640cee18cb2abfafa32682570446993e08f9e4d8
Ranchoo28 <savcreaa.kr99@gmail.com>
2023-11-08 22:21:37 +0100

    Caricato esercizio JUnit

---
+++ Progetto/FundAstic/Frontend/src/app/Services/RequestService.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'   // Specifica il tipo di contenuto
+   // Se necessario, aggiungi un'intestazione di autorizzazione
+  })
+};
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestService {
+  loginUrl = "http://localhost:8080/api/login"
+  registrationUrl = "http://localhost:8080/api/registration"
+
+  constructor(private http: HttpClient) { }
+  loginUtente(data: {}) {
+    return this.http.post(this.loginUrl, data, httpOptions).subscribe()
+  }
+
+  
+ 
+  insertUtente(data: {}){
+    return this.http.post(this.registrationUrl, data,httpOptions).subscribe()
+  }
+}
